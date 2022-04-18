992e5d8b4e2f40b97a2b68aeca162f390c48ee45
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-18 13:59:11 +0100

    chore(app): setup performance plugin on storybook

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,3 +1,6 @@
+import { addDecorator } from "@storybook/react";
+import { withPerformance } from "storybook-addon-performance";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +10,5 @@ export const parameters = {
     },
   },
 };
+
+addDecorator(withPerformance);
