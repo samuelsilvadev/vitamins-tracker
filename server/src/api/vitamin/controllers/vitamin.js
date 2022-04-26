860ce8288b2f65282b8bb8f20b8e0df0c4592c65
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-26 19:41:25 +0100

    feat(server): create schema for food and vitamins

---
+++ server/src/api/vitamin/controllers/vitamin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  vitamin controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::vitamin.vitamin');
---
+++ server/src/api/vitamin/routes/vitamin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vitamin router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::vitamin.vitamin');
