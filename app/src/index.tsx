18222e68d6c1f90e48ac1113d9a3381701fedc28
Samuel Silva <samuelsilvawb@gmail.com>
2022-07-08 22:57:57 +0100

    chore(app): Setup react-query

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./App";
 
 import "@wonderflow/react-components/core.css";
 import "@wonderflow/themes";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
